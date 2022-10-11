8c2f0b1a7da1b66a16cf24613cb02ce49b24c3fa
mikaylawalsh <mikayla_walsh@brown.edu>
2022-10-10 23:16:30 -0400

    10/10-17

--- lost.s
+++ lost.s
@@ -1,26 +1,3 @@
-# want to return cookie for test_exploit without disturbing stack 
-# check that base and stack pointer are the same as before -- need 
-# to check or just set 
-# put address of start of buffer in return address spot 
-
-# question: what is starting address of buffer? 
-
-# idea: use this code for exploit code -- turn into hex code -- 
-# and then put buffer address at return address at byte 89
-
-# exploit:
-    # push %rsp
-    # push %rbp 
-    # mov %rsp,%rbp 
-    # sub $0x20,%rsp 
-    # 20 is random num -- figure out how much to adjust by after we write and compile
-    # mov $0x622524ed,%rax  # cookie 
-
-    #fix rbp and rsp - found in gdb - push rsp onto stack
-    # pop %rsp 
-    # pop %rbp 
-    # ret
-
 exploit:
     mov $0x622524ed,%rax
     ret 
